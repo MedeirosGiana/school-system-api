6e12ec8b517ca7550f2da624a088ee95d8c8f45d
giana medeiros <gianamedeiros.00510@gmail.com>
2023-04-24 22:25:03 -0300

    chamada servico, registro de componentes

--- api/src/main/java/com/school/system/api/resources/AlunoResource.java
+++ api/src/main/java/com/school/system/api/resources/AlunoResource.java
@@ -2,19 +2,34 @@ package com.school.system.api.resources;
 
 import com.school.system.api.entities.Aluno;
 import com.school.system.api.repositories.AlunoRepository;
+import com.school.system.api.services.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/alunos")
 public class AlunoResource {
 
     @Autowired
-    private AlunoRepository repository;
-    public ResponseEntity<Aluno> findAll(){
-        Aluno aluno = new Aluno();
-        return ResponseEntity.ok().body(aluno);
+    private AlunoService service;
+
+    @GetMapping
+    public ResponseEntity<List<Aluno>> findAll(){
+        List<Aluno> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Aluno> findById(@PathVariable Integer id){
+        Aluno obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+
 }
---
+++ api/src/main/java/com/school/system/api/services/AlunoService.java
@@ -0,0 +1,26 @@
+package com.school.system.api.services;
+
+import com.school.system.api.entities.Aluno;
+import com.school.system.api.repositories.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository repository;
+
+    public List<Aluno> findAll(){
+        return repository.findAll();
+    }
+
+    public Aluno findById(Integer id){
+       Optional <Aluno> obj = repository.findById(id);
+       return obj.get();
+    }
+
+}
